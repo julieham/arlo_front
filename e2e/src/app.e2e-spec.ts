bd32556e1d5f0c05324ec6fe84095f7f59d0b4c2
Julie Hamadou <dolitham@hotmail.com>
2018-12-23 16:22:58 +0100

    name change to arlo_front

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to travel!');
+    expect(page.getParagraphText()).toEqual('Welcome to arlo_front!');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,12 +16,12 @@ describe('AppComponent', () => {
   it(`should have as title 'travel'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('travel');
+    expect(app.title).toEqual('arlo_front');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to travel!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to arlo_front!');
   }));
 });
