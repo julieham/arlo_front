360e21b0a4eac48e5551793af1c3c07a511226e1
Julie Hamadou <dolitham@hotmail.com>
2019-02-07 13:28:56 +0100

    remodel form create transaction with Angular : The real deal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {  MatDatepickerModule,
+          MatNativeDateModule,
+          MatFormFieldModule,
+          MatInputModule,
+          MatRadioModule,
+          MatSelectModule,
+          MatButtonModule} from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { TransactionsComponent } from './transactions/transactions.component';
@@ -31,7 +40,15 @@ library.add(fas);
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
